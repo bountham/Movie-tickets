09db1b7b7586d3f35ca17fba17d26d5700278769
tommy bounthma & david eliason <student@epicodus.com>
2015-04-15 16:24:01 -0700

    Add test and function for getting customer age and movie title

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
 //set prototypes
 //
-// var Movie = {
-//     name: name_input,
-//     time: time_input,
-//     date: date_input,
-//     // price: ?
-//
-//     }
-//
-//
-// var Customer = {
-//     age: age_input,
-//     name: name_input
-// }
+var Movie = {
+    name: "",
+    time: "",
+    date: "",
+    get_name : function(){
+    return this.name;
+    }
+
+};
+
+
+var Customer = {
+    age : "",
+    get_age : function(){
+    return this.age;
+    }
+}
+
+// var movieStarWars = Object.create(Movie);
+// var movieGladiator = Object.create(Movie);
+// var movieForrestGump = Object.create(Movie);
+// var movieSpartan = Object.create(Movie);
+var moviePicked = Object.create(Movie);
+
+var customer1 = Object.create(Customer);
+
+
 
 //grab data
 
@@ -20,13 +34,17 @@ $(document).ready(function(){
     $("form#movies").submit(function(event){
         event.preventDefault();
 
-var name_input = $("select#movie-name").val();
+    var name_input = $("select#movie-name").val();
+    var time_input = $("select#movie-time").val();
+    var date_input = $("#datepicker").val();
+    var customer_age = $("input#age").val();
 
-var time_input = $("select#movie-time").val();
+customer1.age = customer_age;
+moviePicked.name = name_input;
+moviePicked.time = time_input;
+moviePicked.date = date_input;
 
-var date_input = $("#datepicker").val();
-
-var customer_age = $("#age").val();
+alert(moviePicked.name);
 
     });
 
--- specs/specs.js
+++ specs/specs.js
@@ -1,9 +1,31 @@
-describe("Movie", function(){
-    describe("select-movie", function() {
-        it('will return Star Wars when asked for the movie name', function(){
+describe("Movie", function()
+{
+    describe("select_movie", function()
+    {
+        it('will return Star Wars when asked for the movie name', function()
+        {
             var testSelectMovie = Object.create(Movie);
             testSelectMovie.name = "star-wars";
-            expect(testSelectMovie.select-movie()).to.equal("star-wars");
+            expect(testSelectMovie.get_name()).to.equal("star-wars");
         });
+
+        describe("get_age", function()
+         {
+
+        it('will return customer age when asked for the age', function()
+        {
+            var testCustomer = Object.create(Customer);
+            testCustomer.age = 33;
+            expect(testCustomer.get_age()).to.equal(33);
+        });
+
+
+
+
+
+
     });
+
+});
+
 });
